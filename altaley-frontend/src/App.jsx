369de05693c816c0f536f249ce6afce93e23a155
SaadAlsayed <smqaa.github@gmail.com>
2024-02-13 19:07:55 +0300

    .

--- altaley-frontend/src/App.jsx
+++ altaley-frontend/src/App.jsx
@@ -1,62 +1,27 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import NavBar from "./components/NavBar";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
 function App() {
 
 
-  const [links, setLinks] = useState("");
+  // const [links, setLinks] = useState("");
 
-  useEffect(() => {
-    axios.get('/')
-      .then(response => {
-        setLinks(response.data.links);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios.get('/')
+  //     .then(response => {
+  //       setLinks(response.data.links);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <>
 
       {/* navigation bar */}
-      <nav className="navbar fixed-top h-10">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">شعار تالي</a>
-
-          <div className="d-flex ">
-            <div className="dropend">
-              <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">المظهر الليلي</a></li>
-                <li><a className="dropdown-item" target="_blank" href="https://github.com/saad-als/altaley" rel="noreferrer">Github</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-      </nav>
-
-      {/* middle and main container */}
-      <div className="main-container  container-fluid">
-
-        <div className="d-flex justify-content container-md border">
-          <p> {links}</p>
-        </div>
-
-        <div className="showing-box">
-
-
-
-
-        </div>
-
-
-
-
-
-      </div>
+      <NavBar />
 
     </>
   )
