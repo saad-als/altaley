98101547550d1aaeee9c9a92e378a142e8f24d92
SaadAlsayed <smqaa.github@gmail.com>
2024-02-15 08:42:23 +0300

    adding a list of videos to be shown when user send the category list

--- altaley-frontend/src/components/SuggestedVideos.jsx
+++ altaley-frontend/src/components/SuggestedVideos.jsx
@@ -1,6 +1,13 @@
+import VideoCard from "./VideoCard";
 function SuggestedVideos() {
     return (
         <>
+            <div className="list-group">
+                <button type="button" className="list-group-item list-group-item-action"> <VideoCard /> </button>
+                <button type="button" className="list-group-item list-group-item-action"> <VideoCard /> </button>
+                <button type="button" className="list-group-item list-group-item-action"> <VideoCard /> </button>
+
+            </div>
         </>
     );
 }
