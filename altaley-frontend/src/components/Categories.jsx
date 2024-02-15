b393c0ee8e2f38ee5a89bd9562ca4c2a2ce1b747
SaadAlsayed <smqaa.github@gmail.com>
2024-02-15 08:54:50 +0300

    basic category list

--- altaley-frontend/src/components/Categories.jsx
+++ altaley-frontend/src/components/Categories.jsx
@@ -1,6 +1,31 @@
 function Categories() {
     return (
         <>
+
+            <div className="d-inline p-2">
+                <button type="button" className="btn btn-outline-secondary">Title</button>
+                <button type="button" className="btn btn-outline-secondary">Title</button>
+                <button type="button" className="btn btn-outline-secondary">Title</button>
+
+                <button type="button" className="btn btn-outline-secondary">Title</button>
+                <button type="button" className="btn btn-outline-secondary">Title</button>
+                <button type="button" className="btn btn-outline-secondary">Title</button>
+            </div>
+
+
+            {/* <ul className="list-group">
+                <li className="list-group-item">
+                    <button type="button" className="btn btn-outline-secondary">Secondary</button>
+                </li>
+
+                <li className="list-group-item">
+                    <button type="button" className="btn btn-outline-secondary">Secondary</button>
+                </li>
+
+                <li className="list-group-item">
+                    <button type="button" className="btn btn-outline-secondary">Secondary</button>
+                </li>
+            </ul> */}
         </>
     );
 }
