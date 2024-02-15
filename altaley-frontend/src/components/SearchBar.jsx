2fc1f58a2ea2d703c82313c566cf8bb08235ac95
SaadAlsayed <smqaa.github@gmail.com>
2024-02-15 08:46:05 +0300

    this what the search bar will look like, all styling will be added later after i connect the backend with the frontend

---
+++ altaley-frontend/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+function SearchBar() {
+    return (
+        <>
+            <form action="POST" className="d-flex">
+                <button className="" type="submit">ارسل</button>
+                <input type="text" className="border " />
+            </form>
+        </>
+    );
+}
+
+export default SearchBar
