367a7568b72d8d9c6f5315c0edb9d4a227d8029f
SaadAlsayed <smqaa.github@gmail.com>
2024-02-24 10:06:51 +0300

    cleaning up

--- altaley-frontend/src/App.jsx
+++ altaley-frontend/src/App.jsx
@@ -1,52 +1,10 @@
-import NavBar from "./components/NavBar";
-import SearchBar from "./components/SearchBar";
-import Categories from "./components/Categories";
-// import SuggestedVideos from "./components/SuggestedVideos";
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
+import HomePage from "./pages/HomePage"
 function App() {
 
 
-  // const [links, setLinks] = useState("");
-
-  // useEffect(() => {
-  //   axios.get('/')
-  //     .then(response => {
-  //       setLinks(response.data.links);
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     });
-  // }, []);
-
   return (
     <>
-
-      {/* navigation bar */}
-      <div className="container d-block p-2">
-        <NavBar />
-
-        <div className="container">
-          <p>أهلا بك في (تالي) اختر التصنيفات التي تعجبك وتالي يرشح لك فيدوهات ممتعة لتشاهدها.</p>
-
-          <div className="main-section container p-4">
-            <SearchBar />
-            {/* <SuggestedVideos /> */}
-
-
-          </div>
-
-          <div className="categories-section container p-4">
-            <Categories />
-
-          </div>
-
-
-        </div>
-
-      </div>
-
+      <HomePage />
     </>
   )
 }
--- altaley-frontend/src/components/Categories.jsx
+++ altaley-frontend/src/components/Categories.jsx
@@ -1,31 +1,11 @@
+// import categoriesList from "../categoriesList.json";
+
 function Categories() {
+
+
     return (
         <>
 
-            <div className="d-inline p-2">
-                <button type="button" className="btn btn-outline-secondary">Title</button>
-                <button type="button" className="btn btn-outline-secondary">Title</button>
-                <button type="button" className="btn btn-outline-secondary">Title</button>
-
-                <button type="button" className="btn btn-outline-secondary">Title</button>
-                <button type="button" className="btn btn-outline-secondary">Title</button>
-                <button type="button" className="btn btn-outline-secondary">Title</button>
-            </div>
-
-
-            {/* <ul className="list-group">
-                <li className="list-group-item">
-                    <button type="button" className="btn btn-outline-secondary">Secondary</button>
-                </li>
-
-                <li className="list-group-item">
-                    <button type="button" className="btn btn-outline-secondary">Secondary</button>
-                </li>
-
-                <li className="list-group-item">
-                    <button type="button" className="btn btn-outline-secondary">Secondary</button>
-                </li>
-            </ul> */}
         </>
     );
 }
