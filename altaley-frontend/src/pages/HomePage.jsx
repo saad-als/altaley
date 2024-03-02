711997ec262ed193afea206cb3eca3c0c890016a
SaadAlsayed <smqaa.github@gmail.com>
2024-03-02 17:31:53 +0300

    restructuring the project front end and backend from the scratch

--- altaley-frontend/src/pages/HomePage.jsx
+++ altaley-frontend/src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
-import Categories from "../components/Categories";
+import axios from 'axios';
+import NavBar from "../components/NavBar";
+import { useState, useEffect } from 'react';
 function HomePage() {
+
+    const [data, setData] = useState(null)
+
+
+    const getData = async () => {
+        const data = await axios.get("http://localhost:5000");
+        setData(data)
+    };
     return (
         <>
-            <Categories />
+            <NavBar />
+            <button type="button" className="btn btn-info" onClick={getData}>ارسال</button>
+
+
         </>
     );
 }
